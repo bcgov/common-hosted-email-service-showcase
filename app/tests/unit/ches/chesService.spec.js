4bda8444e1911308c96865dc49daf31ba481ba39
Jeremy Ho <jujaga@gmail.com>
2021-04-21 14:20:56 -0700

    Fix existing chesService unit tests
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- app/tests/unit/ches/chesService.spec.js
+++ app/tests/unit/ches/chesService.spec.js
@@ -1,45 +1,67 @@
+const axios = require('axios');
+const config = require('config');
+const MockAdapter = require('axios-mock-adapter');
+
 const helper = require('../../common/helper');
 const chesService = require('../../../src/ches/chesService');
+const errorToProblem = require('../../../src/components/errorToProblem');
+
+const apiUrl = `${config.get('serviceClient.commonServices.ches.endpoint')}/v1`;
+const mockInstance = axios.create();
+const mockAxios = new MockAdapter(mockInstance);
+
+chesService.axios = mockInstance;
+jest.mock('../../../src/components/errorToProblem');
 
 helper.logHelper();
 
-describe('health', () => {
-
-  it('should call CHES service to get health', async () => {
-
-    chesService.health = jest.fn().mockReturnValue({ data: 'test', status: 200 });
-    const { data, status } = await chesService.health();
-
-    expect(data).toBeTruthy();
-    expect(status).toBe(200);
-    expect(chesService.health).toHaveBeenCalledTimes(1);
+describe('chesService', () => {
+  beforeEach(() => {
+    mockAxios.reset();
+    errorToProblem.mockReset();
   });
 
-});
+  describe('health', () => {
+    const endpoint = `${apiUrl}/health`;
 
+    it('should call CHES health endpoint successfully', async () => {
+      mockAxios.onGet(endpoint).reply(200, {});
+      const result = await chesService.health();
 
-describe('send', () => {
+      expect(result.data).toBeTruthy();
+      expect(result.status).toBe(200);
+      expect(mockAxios.history.get).toHaveLength(1);
+      expect(errorToProblem).toHaveBeenCalledTimes(0);
+    });
 
-  // const email = {
-  //   bodyType: 'html',
-  //   body: '<h1>Welcome to Common Services</h1><p>Sent by <a href="https://bcgov.github.io/common-hosted-email-service/">CHES</a></p>',
-  //   delayTS: 0,
-  //   from: 'abc@gov.bc.ca',
-  //   subject: 'CHES Email Message',
-  //   to: ['xyz@gov.bc.ca'],
-  //   tag: 'email_123'
-  // };
+    it('should report a problem', async () => {
+      mockAxios.onGet(endpoint).networkError();
+      await chesService.health();
 
-
-  it('calls CHES service to send email', async () => {
-
-    chesService.send = jest.fn().mockReturnValue({ data: 'test', status: 200 });
-    const { data, status } = await chesService.send({});
-
-    expect(data).toBeTruthy();
-    expect(status).toBe(200);
-    expect(chesService.send).toHaveBeenCalledTimes(1);
+      expect(mockAxios.history.get).toHaveLength(1);
+      expect(errorToProblem).toHaveBeenCalledTimes(1);
+    });
   });
 
-});
+  describe('send', () => {
+    const endpoint = `${apiUrl}/email`;
 
+    it('should call CHES email endpoint successfully', async () => {
+      mockAxios.onPost(endpoint).reply(201, 'test');
+      const result = await chesService.send();
+
+      expect(result.data).toBeTruthy();
+      expect(result.status).toBe(201);
+      expect(mockAxios.history.post).toHaveLength(1);
+      expect(errorToProblem).toHaveBeenCalledTimes(0);
+    });
+
+    it('should report a problem', async () => {
+      mockAxios.onGet(endpoint).networkError();
+      await chesService.send();
+
+      expect(mockAxios.history.post).toHaveLength(1);
+      expect(errorToProblem).toHaveBeenCalledTimes(1);
+    });
+  });
+});
