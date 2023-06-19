7011ea51e16148d1d81eb7032b7253b28d3fa219
Csaky <Tim.Csaky@gov.bc.ca>
2023-06-19 13:54:04 -0700

    Update api route mounting

--- app/src/routes/v1.js
+++ app/src/routes/v1.js
@@ -4,7 +4,7 @@ const ches = require('../ches');
 const chesPath = ches.mount(router);
 
 // Base v1 responder
-router.get('/', (_req, res) => {
+router.get('/app/', (_req, res) => {
   res.status(200).json({
     endpoints: [
       chesPath
