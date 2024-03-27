804d62e4662455813d1ea70711c81104cdd46778
JatinderSingh <jatinder.singh@gov.bc.ca>
2024-03-27 11:33:28 -0700

    Update npm packages

--- app/frontend/lcov-fix.js
+++ app/frontend/lcov-fix.js
@@ -1,13 +1,14 @@
 // Jest 25.x onwards emits coverage reports on a different source path
 // https://stackoverflow.com/q/60323177
-const fs = require('fs');
+import fs from 'fs';
+import process from 'process';
 const file = './coverage/lcov.info';
 
 fs.readFile(file, 'utf8', (err, data) => {
   if (err) {
     return console.error(err); // eslint-disable-line no-console
   }
-  const result = data.replace(/src/g, `${process.cwd()}/src`);
+  const result = data.replace(/SF:/g, `SF:${process.cwd()}/`);
 
   fs.writeFile(file, result, 'utf8', err => {
     if (err) return console.error(err); // eslint-disable-line no-console
